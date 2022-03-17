e8e826077e4ce7a22dd7635b08d116d5b68b1c8b
HOHL <laurent.hohl@gmail.com>
2022-03-17 10:56:12 +0100

    LIBRARY-29 : Update file preview position

--- webapp/js/admin/plugins/library/library.imgPreview.js
+++ webapp/js/admin/plugins/library/library.imgPreview.js
@@ -5,12 +5,12 @@ function readURL(input) {
 	    // test if uploaded file is an image
 	    if ( !imageType.test(file.type) ) return
 	    // create the image if not exists
-		const inputId = "#" + input.id + "_preview"
-		if ( !$( inputId ).length ){ 
+		const inputId = "#" + input.id , inputIdPreview = "#" + input.id + "_preview", parentGroup=$(inputId).parentsUntil('.form-group');
+		if ( !$( inputIdPreview ).length ){ 
 			img = $("<img id='"+ input.id +"_preview' class='thumbnail thumbnail-lg' src='#' ></img>")
-			$("#" + input.id ).after( $("<p></p>").append( img ) )
+			$( parentGroup ).after( $("<p></p>").append( img ) )
 		} else {
-			img = $( inputId )
+			img = $( inputIdPreview )
 		}
 		reader.onload = function(e){ img.attr('src', e.target.result); }
 		reader.readAsDataURL( file )
