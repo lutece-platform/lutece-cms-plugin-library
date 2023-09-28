0d2d8a032fd68bf88d22d1fa97b235faceaea04e
HOHL <laurent.hohl@gmail.com>
2023-09-28 16:34:03 +0200

    LUT-26390 : Update db, remove unused js lib, update and move images and improve templates

--- src/sql/plugins/library/core/init_core_library.sql
+++ src/sql/plugins/library/core/init_core_library.sql
@@ -9,4 +9,12 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 --
 INSERT INTO core_user_right (id_right,id_user) VALUES ('LIBRARY_MANAGEMENT',1); 
 
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image_space';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image_space', '6');
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image', '1');
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf', '3');
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf_space';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf_space', '8');
 
---
+++ src/sql/plugins/library/upgrades/update_db_library_4.0.3-5.0.1.sql
@@ -0,0 +1,8 @@
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image_space';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image_space', '6');
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image', '1');
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf', '3');
+DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf_space';
+INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf_space', '8');
