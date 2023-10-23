2ce9546972d57f4eb0d0299405e56f2da241657d
HOHL <laurent.hohl@gmail.com>
2023-10-23 09:37:29 +0200

    LUT-26390 : Update core_datastore in sql scripts with correct keys

--- src/sql/plugins/library/core/init_core_library.sql
+++ src/sql/plugins/library/core/init_core_library.sql
@@ -8,13 +8,11 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 -- Dumping data for table core_user_right
 --
 INSERT INTO core_user_right (id_right,id_user) VALUES ('LIBRARY_MANAGEMENT',1); 
-
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image_space';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image_space', '6');
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image', '1');
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf', '3');
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf_space';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf_space', '8');
-
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_image';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_image', '1');
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_image_space';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_image_space', '6');
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_pdf';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_pdf', '3');
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_pdf_space';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_pdf_space', '8');
--- src/sql/plugins/library/upgrades/update_db_library_4.0.3-5.0.1.sql
+++ src/sql/plugins/library/upgrades/update_db_library_4.0.3-5.0.1.sql
@@ -1,8 +1,9 @@
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image_space';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image_space', '6');
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_image';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_image', '1');
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf', '3');
-DELETE FROM core_datastore WHERE entity_key='lutece_insert_service_media_type_pdf_space';
-INSERT INTO core_datastore VALUES ('lutece_insert_service_media_type_pdf_space', '8');
+
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_image';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_image', '1');
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_image_space';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_image_space', '6');
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_pdf';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_pdf', '3');
+DELETE FROM core_datastore WHERE entity_key='library.insert_service_media_type_pdf_space';
+INSERT INTO core_datastore VALUES ('library.insert_service_media_type_pdf_space', '8');
